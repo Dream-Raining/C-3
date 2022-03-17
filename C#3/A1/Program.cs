0327b23b48b560f394805f62ac73f4faecba38d8
Dream-Raining <100700655+Dream-Raining@users.noreply.github.com>
2022-03-17 19:16:04 +0800

    Add files via upload

---
+++ C#3/A1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LIST
+{
+
+    internal class Progrem
+    {
+        public delegate double DeleOne(double t1, double t2);
+
+        class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T data { get; set; }
+            public Node(T t)
+            {
+                data = t;
+                Next = null;
+            }
+        }
+        class List<T> where T : IComparable
+        {
+            private Node<T> Head;
+            private Node<T> Tail;
+            public List()
+            {
+                Head = Tail = null;
+            }
+            public Node<T> getHead()
+            {
+                return Head;
+            }
+            public void ADD(T t)
+            {
+                Node<T> node = new Node<T>(t);
+                if (Head == null)
+                {
+                    Head = Tail = node;
+                }
+                else
+                {
+                    Tail.Next = node;
+                    Tail = node;
+                }
+            }
+            public void ForeachList()
+            {
+                DeleOne sum = (double t1, double t2) =>
+                {
+                    return t1 + t2;
+                };
+                Node<T> head = getHead();
+                Node<T> p = head;
+                T min = head.data;
+                T max = head.data;
+                double total = 0;
+                while (p != null)
+                {
+                    if (p.data.CompareTo(max) > 0)
+                    {
+                        max = p.data;
+                    }
+                    if (p.data.CompareTo(min) < 0)
+                    {
+                        min = p.data;
+                    }
+                    total = sum(total, double.Parse(p.data.ToString()));
+                    p = p.Next;
+                }
+                Console.WriteLine(max.ToString());
+                Console.WriteLine(min.ToString());
+                Console.WriteLine(total.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<double> list = new List<double>();
+            list.ADD(1);
+            list.ADD(2);
+            list.ForeachList();
+
+        }
+    }
+}
---
+++ C#3/A2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace EventTest
+{
+
+
+
+    internal class Program
+    {
+        class SecondEvent : EventArgs
+        {
+            public int second;
+            public SecondEvent()
+            {
+                second = 0;
+            }
+            public override string ToString()
+            {
+                return second.ToString();
+            }
+        }
+
+        public delegate void EventEventHandler(object sender, EventArgs e);
+        class AlarmClock
+        {
+
+            public event EventEventHandler Tick;
+            public event EventEventHandler Alarm;
+            public int alarmTime;
+            public AlarmClock()
+            {
+                Console.WriteLine("Please enter the end time");
+                int time = int.Parse(Console.ReadLine().ToString());
+                alarmTime = time;
+                while (alarmTime <= 0)
+                {
+                    Console.WriteLine("Please enter the end time");
+                    time = int.Parse(Console.ReadLine().ToString());
+                    alarmTime = time;
+                }
+
+            }
+            public void Working()
+            {
+                SecondEvent se = new SecondEvent();
+                while (se.second < alarmTime)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    se.second++;
+                    Tick(this, se);
+                }
+                Alarm(this, se);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            AlarmClock alarmClock = new AlarmClock();
+            alarmClock.Alarm += AlarmClock_Alarm;
+            alarmClock.Tick += AlarmClock_Tick;
+            alarmClock.Tick += AlarmClock_Tick1;
+            alarmClock.Working();
+        }
+
+        private static void AlarmClock_Tick1(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("滴答");
+        }
+
+        private static void AlarmClock_Tick(object sender, EventArgs e)
+        {
+            Console.Write(e.ToString());
+        }
+
+        private static void AlarmClock_Alarm(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("END");
+        }
+    }
+}
